910a0631935ff22df385650e516427152f75c3f3
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2024-03-04 11:35:10 +0530

    user model and db

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect('mongodb+srv://raghav:raghav@cluster0.cdhhrci.mongodb.net/assignment?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>
+ {
+ console.log("Connected to Mongo");
+})
+.catch((err)=>{
+console.log(err);
+});
+
+const app =express();
+app.listen(3000, ()=>{
+console.log('Server listening on port 3000!');
+});
