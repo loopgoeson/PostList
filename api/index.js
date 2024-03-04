7b2f40082f08af5d0a838989d6068ddfde8ebedd
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2024-03-04 11:48:33 +0530

    controllers

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
+import  userRoutes from './routes/userRoutes.js';
 
 mongoose
 .connect('mongodb+srv://raghav:raghav@cluster0.cdhhrci.mongodb.net/assignment?retryWrites=true&w=majority&appName=Cluster0')
@@ -17,3 +18,5 @@ const app =express();
 app.listen(3000, ()=>{
 console.log('Server listening on port 3000!');
 });
+
+app.use('/api/user',userRoutes);
